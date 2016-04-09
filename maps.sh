2614a2d328999c10780e59d3c46b5c1d08309097
Robin Appelman <icewind@owncloud.com>
2016-04-09 16:13:59 +0200

    dont download any maps in advance

--- maps.sh
+++ maps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf/maps
 
-maps=("pl_badwater_pro_v9" "koth_ashville_rc1" "pl_swiftwater_ugc" "koth_product_rc8")
+maps=()
 
 for map in ${maps[@]}; do
 	wget -nv "http://fakkelbrigade.eu/maps/$map.bsp"
