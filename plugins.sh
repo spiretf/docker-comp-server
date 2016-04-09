93593a33be5237e2f771feef6000f06f3397fb22
Robin Appelman <icewind@owncloud.com>
2016-04-09 16:14:08 +0200

    cleanup zips

--- plugins.sh
+++ plugins.sh
@@ -6,12 +6,12 @@ wget -nv "https://github.com/Lange/SOAP-TF2DM/archive/master.zip" -O "soap-dm.zi
 unzip soap-dm.zip
 cp -r SOAP-TF2DM-master/* ./
 rm -r SOAP-TF2DM-master
+rm soap-dm.zip
 
 # Socket
 
 wget -nv "https://forums.alliedmods.net/attachment.php?attachmentid=83286&d=1299423920" -O "socket.zip"
 unzip socket.zip
-
 rm socket.zip
 
 # Curl
@@ -19,6 +19,7 @@ cd $HOME/hlserver/tf2/tf/addons/sourcemod
 
 wget -nv "https://sourcemod-curl-extension.googlecode.com/files/curl_1.3.0.0.zip"
 unzip curl_1.3.0.0.zip
+rm curl_1.3.0.0.zip
 
 # Websockets
 
@@ -33,7 +34,6 @@ cd $HOME/hlserver/tf2/tf/addons/sourcemod/plugins
 
 wget -nv  --header="Accept: text/html" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" "http://sourcemod.krus.dk/f2-sourcemod-plugins.zip"
 unzip f2-sourcemod-plugins.zip
-
 rm f2-sourcemod-plugins.zip
 
 # WebRCON
