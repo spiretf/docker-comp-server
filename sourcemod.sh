714ebee13d6c2155671fe60908fc3f21a03dce36
Robin Appelman <icewind@owncloud.com>
2016-05-14 23:29:30 +0200

    bumb metamod and sourcemod versions

--- sourcemod.sh
+++ sourcemod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf
 
-mm_version="1.10.7-git951"
-sm_version="1.8.0-git5825"
+mm_version="1.10.7-git952"
+sm_version="1.8.0-git5891"
 
 mm_url="http://www.metamodsource.net/mmsdrop/1.10/mmsource-${mm_version}-linux.tar.gz"
 sm_url="http://www.sourcemod.net/smdrop/1.8/sourcemod-${sm_version}-linux.tar.gz"
