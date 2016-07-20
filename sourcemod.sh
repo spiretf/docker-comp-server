1a9c029b8856baabf2f479997272593a8f049559
Robin Appelman <icewind@owncloud.com>
2016-07-20 23:19:50 +0200

    auto pick latest metamod and sourcemod

--- sourcemod.sh
+++ sourcemod.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf
 
-mm_version="1.10.7-git952"
-sm_version="1.8.0-git5891"
-
-mm_url="http://www.metamodsource.net/mmsdrop/1.10/mmsource-${mm_version}-linux.tar.gz"
-sm_url="http://www.sourcemod.net/smdrop/1.8/sourcemod-${sm_version}-linux.tar.gz"
+mm_url=$(wget -q -O - "https://www.metamodsource.net/snapshots" | grep -oP -m1 "http://www.metamodsource.net/mmsdrop/[0-9.]+/mmsource-(.*)-linux.tar.gz")
+sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oP -m1 "http://www.sourcemod.net/smdrop/[0-9.]+/sourcemod-(.*)-linux.tar.gz")
 
 wget -nv $mm_url
 wget -nv $sm_url
 
-tar -xvzf "mmsource-${mm_version}-linux.tar.gz"
-tar -xvzf "sourcemod-${sm_version}-linux.tar.gz"
+tar -xvzf mmsource-*-linux.tar.gz
+tar -xvzf sourcemod-*-linux.tar.gz
 
 rm *.tar.gz
