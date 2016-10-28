eb44779a2e7fe49293c4c2ba081b6861e9f30a23
Robin Appelman <robin@icewind.nl>
2016-10-28 20:34:13 +0200

    Fix sm download link scanning

--- sourcemod.sh
+++ sourcemod.sh
@@ -2,7 +2,7 @@
 cd $HOME/hlserver/tf2/tf
 
 mm_url=$(wget -q -O - "https://www.metamodsource.net/snapshots" | grep -oP -m1 "http://www.metamodsource.net/mmsdrop/[0-9.]+/mmsource-(.*)-linux.tar.gz")
-sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oP -m1 "http://www.sourcemod.net/smdrop/[0-9.]+/sourcemod-(.*)-linux.tar.gz")
+sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oP -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-linux.tar.gz")
 
 wget -nv $mm_url
 wget -nv $sm_url
