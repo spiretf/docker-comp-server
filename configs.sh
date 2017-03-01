4b3e1ce2eb73a12fa22dff413813043a7c1fefa7
Robin Appelman <robin@icewind.nl>
2017-03-01 21:14:44 +0100

    fix ugc config unzipping

--- configs.sh
+++ configs.sh
@@ -5,8 +5,6 @@ ugc_cfg_version_9=$(wget -O - "http://ugcleague.com/files_tf2h.cfm" | grep -oP "
 ugc_cfg_version_6=$(wget -O - "http://ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6}")
 ugc_cfg_version_4=$(wget -O - "http://ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
 
-echo $ugc_cfg_version_9
-
 wget -nv "http://etf2l.org/configs/etf2l_configs.zip"
 unzip etf2l_configs.zip
 
@@ -15,7 +13,7 @@ wget -nv "http://ugcleague.com/files/configs/UGC_6v6_cfg_${ugc_cfg_version_6}.zi
 wget -nv "http://ugcleague.com/files/configs/UGC_4v4_cfg_and_whitelist_${ugc_cfg_version_4}.zip"
 
 unzip -o UGC_HL_cfg_and_whitelist_${ugc_cfg_version_9}.zip
-unzip -o UGC_6v6_cfg_and_whitelist_${ugc_cfg_version_6}.zip
+unzip -o UGC_6v6_cfg_${ugc_cfg_version_6}.zip
 unzip -o UGC_4v4_cfg_and_whitelist_${ugc_cfg_version_4}.zip
 
 rm *.zip
