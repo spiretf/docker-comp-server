001f4c4bf32a77a200694cde71409285f7bd7d3e
Robin Appelman <robin@icewind.nl>
2017-03-01 20:07:29 +0100

    fix downloading ugc 6s config

--- configs.sh
+++ configs.sh
@@ -2,7 +2,7 @@
 cd $HOME/hlserver/tf2/tf/cfg
 
 ugc_cfg_version_9=$(wget -O - "http://ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
-ugc_cfg_version_6=$(wget -O - "http://ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
+ugc_cfg_version_6=$(wget -O - "http://ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6}")
 ugc_cfg_version_4=$(wget -O - "http://ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
 
 echo $ugc_cfg_version_9
@@ -11,7 +11,7 @@ wget -nv "http://etf2l.org/configs/etf2l_configs.zip"
 unzip etf2l_configs.zip
 
 wget -nv "http://ugcleague.com/files/configs/UGC_HL_cfg_and_whitelist_${ugc_cfg_version_9}.zip"
-wget -nv "http://ugcleague.com/files/configs/UGC_6v6_cfg_and_whitelist_${ugc_cfg_version_6}.zip"
+wget -nv "http://ugcleague.com/files/configs/UGC_6v6_cfg_${ugc_cfg_version_6}.zip"
 wget -nv "http://ugcleague.com/files/configs/UGC_4v4_cfg_and_whitelist_${ugc_cfg_version_4}.zip"
 
 unzip -o UGC_HL_cfg_and_whitelist_${ugc_cfg_version_9}.zip
