2999ac732a613c72848851f1905065eea16a6b5a
Robin Appelman <robin@icewind.nl>
2019-01-30 19:51:51 +0100

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf/cfg
 
-ugc_cfg_version_9=$(wget -O - "http://ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
-ugc_cfg_version_6=$(wget -O - "http://ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6}")
-ugc_cfg_version_4=$(wget -O - "http://ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6}")
+ugc_cfg_version_9=$(wget -O - "http://ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_6=$(wget -O - "http://ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_4=$(wget -O - "http://ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
 
 wget -nv "http://etf2l.org/configs/etf2l_configs.zip"
 unzip etf2l_configs.zip
