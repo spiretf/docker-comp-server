7bb66f9e6b3359b0ed2c212c7e53121519fc6265
Robin Appelman <robin@icewind.nl>
2020-05-20 23:02:32 +0200

    use stable sourcemod branch

--- sourcemod.sh
+++ sourcemod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf
 
-mm_url=$(wget -q -O - "https://www.metamodsource.net/downloads.php?branch=dev" | grep -oP -m1 "https://[a-z.]+/mmsdrop/[0-9.]+/mmsource-(.*)-linux.tar.gz")
-sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oP -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-linux.tar.gz")
+mm_url=$(wget -q -O - "https://www.metamodsource.net/downloads.php?branch=stable" | grep -oP -m1 "https://[a-z.]+/mmsdrop/[0-9.]+/mmsource-(.*)-linux.tar.gz")
+sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=stable" | grep -oP -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-linux.tar.gz")
 
 wget -nv $mm_url
 wget -nv $sm_url
