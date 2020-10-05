550a082c918068aea14f13e1e2c5b941b27b7f17
Robin Appelman <robin@icewind.nl>
2020-10-05 20:34:27 +0200

    fix ugc download

--- configs.sh
+++ configs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cd $HOME/hlserver/tf2/tf/cfg
 
-ugc_cfg_version_9=$(wget -O - "https://www.ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
-ugc_cfg_version_6=$(wget -O - "https://www.ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
-ugc_cfg_version_4=$(wget -O - "https://www.ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_and_whitelist_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_9=$(wget -q -O - "https://www.ugcleague.com/files_tf2h.cfm" | grep -oP "UGC_HL_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_6=$(wget -q -O - "https://www.ugcleague.com/files_tf26.cfm" | grep -oP "UGC_6v6_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
+ugc_cfg_version_4=$(wget -q -O - "https://www.ugcleague.com/files_tf24.cfm" | grep -oP "UGC_4v4_cfg_(.*)\.zip" | grep -m1 -oP "v\d{6,8}")
 
 wget -nv "http://etf2l.org/configs/etf2l_configs.zip"
 unzip etf2l_configs.zip
 
-wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_HL_cfg_and_whitelist_${ugc_cfg_version_9}.zip"
-wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.net/files/configs/UGC_6v6_cfg_and_whitelist_${ugc_cfg_version_6}.zip"
-wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_4v4_cfg_and_whitelist_${ugc_cfg_version_4}.zip"
+wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_HL_cfg_${ugc_cfg_version_9}.zip"
+wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_6v6_cfg_${ugc_cfg_version_6}.zip"
+wget -nv --header "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11" "https://www.ugcleague.com/files/configs/UGC_4v4_cfg_${ugc_cfg_version_4}.zip"
 
-unzip -oj UGC_HL_cfg_and_whitelist_${ugc_cfg_version_9}.zip
-unzip -o UGC_6v6_cfg_and_whitelist_${ugc_cfg_version_6}.zip
-unzip -o UGC_4v4_cfg_and_whitelist_${ugc_cfg_version_4}.zip
+unzip -oj UGC_HL_cfg_${ugc_cfg_version_9}.zip
+unzip -oj UGC_6v6_cfg_${ugc_cfg_version_6}.zip
+unzip -oj UGC_4v4_cfg_${ugc_cfg_version_4}.zip
 
 # dont set the server and stv name
 sed -i '/hostname/d' ugc_*_custom.cfg
